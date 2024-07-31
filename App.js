82f1a64809d2e297a29f637eb3be2dd8a9ba52a5
DorlixitoG <vbkevin.iesanpablo@gmail.com>
2024-07-31 00:31:31 -0500

    ya da

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-
-// Habilita el uso de pantallas nativas en React Native para mejorar el rendimiento
 import { enableScreens } from "react-native-screens";
-
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import Screens from "./navigation/Screens";
 
 enableScreens();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Screens />
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Screens />
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
